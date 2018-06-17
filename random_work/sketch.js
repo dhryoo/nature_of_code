1606f1cbee33cab7b844bff3676554cf3ee38d4b
Doohyun Ryoo <idfeelme@gmail.com>
2018-06-17 18:09:38 +0900

    random_work

---
+++ random_work/sketch.js
@@ -0,0 +1,38 @@
+var w           = null;
+var point_color = null;
+function setup()
+{
+    createCanvas(600,600);
+    w = new Walker();
+    background(51);
+} 
+
+function draw()
+{
+    w.walk();
+    w.display();
+}
+
+var Walker = function()
+{
+    this.x = width/2;
+    this.y = height/2;
+}
+
+Walker.prototype.display = function()
+{
+    point_color = color(random(255),random(255),random(255));//,0.5);
+    stroke(point_color);
+    strokeWeight(4);
+    point(this.x,this.y);
+};
+Walker.prototype.walk = function()
+{
+    var choice = floor(random(4));
+    if(choice === 0) this.x+=4;
+    else if(choice === 1) this.x-=4;
+    else if(choice === 2) this.y+=4;
+    else if(choice === 3) this.y-=4;
+    point(this.x,this.y);
+};
+
